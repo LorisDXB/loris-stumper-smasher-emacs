beb96ef71d4ab42e2d616ee1a0a0fa95dc6630f7
Loris <loris.gode@epitech.eu>
2024-03-05 17:24:44 +0100

    UPDATED init.el (added comments)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,3 @@
-;; Set up package.el and MELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -9,16 +8,15 @@
             (setq tab-width 4)
             (setq c-basic-offset 4)))
 
-;; Use spaces instead of tabs for indentation
 (setq-default indent-tabs-mode nil)
-(setq-default tab-width 4) ; Set the desired width of a tab (in spaces)
-(setq-default c-basic-offset 4) ; Set the default indentation for C-like modes
+(setq-default tab-width 4) 
+(setq-default c-basic-offset 4) 
 
-;; Display a vertical line at column 80
 (setq-default display-fill-column-indicator-column 80)
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
 
-;; Install required packages  ADD evil HERE IN THE PACKAGES TO ADD VIM KEYBINDINGS
+;; Install required packages  REMOVE evil HERE IN THE PACKAGES TO REMOVE VIM KEYBINDINGS
+;; NOTE: THIS WILL DESTROY ALL THE SHORTCUTS I MADE SO YOU'LL PROBS HAVE TO REDO THEM
 (defvar my-packages '(use-package fzf evil general neotree vterm all-the-icons doom-themes))
 
 (dolist (pkg my-packages)
@@ -26,89 +24,66 @@
     (package-refresh-contents)
     (package-install pkg)))
 
-;; Set up use-package
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Enable line numbers
 (global-display-line-numbers-mode)
 (setq display-line-numbers-type 'relative)
 
-;; Enable word wrap
 (global-visual-line-mode t)
 
-;; Set up auto-pairs
 (use-package smartparens
   :ensure t
   :config
   (smartparens-global-mode 1))
 
-;; Set up doom-themes
 (use-package doom-themes
   :ensure t
   :config)
   
-
-;; Set up neotree
 (use-package neotree
   :ensure t
   :bind ("<leader>T" . neotree-toggle))
 
-;; Set up fzf
 (use-package fzf
   :ensure t
   :bind ("<leader>." . fzf))
 
-;; Set up evil mode (UNCOMMENT IF YOU USE EVIL)
+;; IF YOU UNTOGGLED evil IN THE PACKAGES, REMOVE ALL THIS SECTION
 (use-package evil
   :ensure t
   :init
   (evil-mode 1))
+;;UNTIL HERE
 
-;; Set up syntastic (you can use flycheck as an alternative)
 (use-package flycheck
   :ensure t
   :config
   (global-flycheck-mode))
 
-;; Set up smooth scrolling
 (use-package smooth-scrolling
   :ensure t
   :config
   (smooth-scrolling-mode 1))
 
-;; Set up doom-modeline for a minimalistic status line
 (use-package doom-modeline
   :ensure t
   :init
   (doom-modeline-mode 1))
 
+;;THESE ARE THE CURRENT KEYBINDS
 (general-define-key
  :states '(normal visual insert emacs)
  :prefix "SPC"
  :non-normal-prefix "M-SPC"
- ;; Define your key bindings here
- ;; For example:
- "." 'fzf
+ "." 'fzf ;;fzf doesnt work mb
  "t" 'neotree-toggle
  "f" 'vterm
  "SPC" 'other-window
  )
-;; Set background based on time
-(if (< (string-to-number (format-time-string "%H")) 12)
-    (load-theme 'doom-one-light t)
-  (load-theme 'doom-one t))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(doom-modeline smooth-scrolling flycheck smartparens doom-themes all-the-icons neotree fzf evil)))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
